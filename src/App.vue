62155a56564c0acd46b42ff371f15e9dff2d0d0b
Kim Tiedemann <kim@tiede.dk>
2023-02-11 01:14:46 +0100

    Added weather service

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <div>Hello {{value}}</div>
+    <div>Current Weather in Copenhagen: {{currentWeather.temp_c}}</div>
+    <div>Current Weather in Copenhagen (feels like): {{currentWeather.feelslike_c}}</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+
   data() {
     return {
-      value: "World"
+      value: "World",
+      currentWeather: {},
     };
+  },
+
+  methods: {
+    async getWeatherData() {
+      const res = await fetch('https://api.weatherapi.com/v1/current.json?key=3de6c9f8c2504acaa36202353230902&q=Copenhagen&aqi=no');
+      const finalRes = await res.json();
+      this.currentWeather = finalRes.current;
+    }
+  },
+  mounted() {
+    this.getWeatherData()
   }
 };
+
 </script>
